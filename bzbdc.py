eab0001e87126d22871f882f38c02681697eefe3
Vladimir Jimenez <allejo@users.noreply.github.com>
2015-09-16 17:24:59 -0700

    Support @include functionality and created parse()

--- bzbdc.py
+++ bzbdc.py
@@ -17,16 +17,22 @@ groups = {}
 # Regex definitions
 varname = "\$[a-zA-Z]+"
 groupname = "[A-Z_\-\.]+"
+comment = "\s*?#.*"
 
 var_name = re.compile("(" + varname + ")")
+comment_line = re.compile(comment)
 var_instantiation = re.compile("(" + varname + ")\s?=\s?\"(.+)\"")
 group_declaration = re.compile("(" + groupname + ")")
 
 
 # Functions
-def loadfile(path):
-    with open(path) as f:
-        return f.read().splitlines()
+def loadfile (path):
+    try:
+        with open(path) as f:
+            return f.read().splitlines()
+    except IOError:
+        print "Fatal: File not found", inputfile
+        sys.exit(2)
 
 def set_variable(name, value):
     global variables
@@ -86,8 +92,58 @@ def handle_permission(group, perm):
         except:
             pass
 
-    if negate_perm not in groups[group]:
+    if negate_perm not in groups[group] and perm not in groups[group]:
         groups[group].append(perm)
+
+def parse(filepath):
+    filecontents = loadfile(filepath)
+    line_counter = 0
+    last_group = ""
+
+    for line in filecontents:
+        line_counter = line_counter + 1
+
+        if line.isspace() or len(line) == 0:
+            continue
+
+        if comment_line.search(line) is not None:
+            continue
+        elif var_instantiation.match(line):
+            vardata = re.search(var_instantiation, line)
+            name = vardata.group(1)
+            value = vardata.group(2)
+
+            set_variable(name, value)
+            continue
+
+        if var_name.search(line) is not None:
+            line = evaluate_variables(line)
+
+        if group_declaration.match(line):
+            last_group = create_group_if_not_exist(line)
+            continue
+
+        line = line.strip()
+
+        if line[:1] == "+" or line[:1] == "-" or line[:1] == "!":
+            handle_permission(last_group, line)
+        elif line[:1] == "@":
+            tokens = line.split(" ")
+            func_call = tokens[0][1:]
+
+            if len(tokens) >= 1:
+                params = tokens[1:]
+            else:
+                params = None
+
+            params.insert(0, last_group)
+
+            try:
+                functions[func_call](params)
+            except KeyError:
+                print "Undefined function: @" + func_call + " on line", line_counter, "of", inputfile
+                sys.exit(2)
+
 
 # Language functions
 def func_include(file_path):
@@ -96,7 +152,7 @@ def func_include(file_path):
     file_path = file_path[1]
 
     try:
-        print loadfile(file_path)
+        parse(file_path)
     except IOError:
         print "Fatal: Included file '" + file_path + "' not found on line", line_counter
         sys.exit(2)
@@ -133,58 +189,6 @@ for opt, arg in opts:
 
 
 # Load the initial file
-try:
-    filecontents = loadfile(inputfile)
-except IOError:
-    print "Fatal: File not found:", inputfile
-    sys.exit(2)
-
-
-# Main loop
-last_group = ""
-
-for line in filecontents:
-    line_counter = line_counter + 1
-
-    if line.isspace() or len(line) == 0:
-        continue
-
-    if line[:1] == "#":
-        continue
-    elif var_instantiation.match(line):
-        vardata = re.search(var_instantiation, line)
-        name = vardata.group(1)
-        value = vardata.group(2)
-
-        set_variable(name, value)
-        continue
-
-    if var_name.search(line) is not None:
-        line = evaluate_variables(line)
-
-    if group_declaration.match(line):
-        last_group = create_group_if_not_exist(line)
-        continue
-
-    line = line.strip()
-
-    if line[:1] == "+" or line[:1] == "-" or line[:1] == "!":
-        handle_permission(last_group, line)
-    elif line[:1] == "@":
-        tokens = line.split(" ")
-        func_call = tokens[0][1:]
-
-        if len(tokens) >= 1:
-            params = tokens[1:]
-        else:
-            params = None
-
-        params.insert(0, last_group)
-
-        try:
-            functions[func_call](params)
-        except KeyError:
-            print "Undefined function: @" + func_call + " on line", line_counter
-            sys.exit(2)
+parse(inputfile)
 
 print groups
